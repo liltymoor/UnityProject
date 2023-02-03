a4461a9f086d27c06151a31c5350fb548bef8706
Timur Chelter <timur.chelter@mail.ru>
2023-02-03 17:31:36 +0300

    week23 addon
    / added comments to Executor class
    / compiled 5sec.exe to run demo around big calculations (5sec.exe are just loops with calculations that lasts for 5 secs)
    /added async method to wait for answer

--- Assets/CalculatorExecutor.cs
+++ Assets/CalculatorExecutor.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Diagnostics;
 using System.Threading;
 using System;
+using System.Threading.Tasks;
 
 public class CalculatorExecutor : MonoBehaviour
 {
@@ -11,24 +12,33 @@ public class CalculatorExecutor : MonoBehaviour
     public TMPro.TextMeshProUGUI value2;
     public TMPro.TMP_Dropdown operation;
 
+
+    private string returned = String.Empty;
+
+    private Process proc;
     public void ExeCalculate()
     {
         if (value1.text == String.Empty || value2.text == String.Empty)
             return;
 
-        Process proc = new Process();
-        proc.StartInfo.FileName = Application.dataPath + "/Resources/C++/calculator.exe";
+        proc = new Process();
+
+        //Setting up .exe path and passing arguments to it
+        //proc.StartInfo.FileName = Application.dataPath + "/Resources/C++/calculator.exe";
+        proc.StartInfo.FileName = Application.dataPath + "/Resources/C++/5sec.exe";
         proc.StartInfo.Arguments +=
             "!" + operation.options[operation.value].text + " "                      // operation
             + value1.GetParsedText().Remove(value1.GetParsedText().Length - 1) + " " // value #1 (Removing null-terminator and passing original value)
             + value2.GetParsedText().Remove(value2.GetParsedText().Length - 1) ;     // value #2 (Removing null-terminator and passing original value)
 
         UnityEngine.Debug.Log("Passed arguments: [ " + proc.StartInfo.Arguments + " ] ");
+
+        //Setting up process properties
         proc.StartInfo.CreateNoWindow = true;
         proc.StartInfo.RedirectStandardOutput = true;
         proc.StartInfo.UseShellExecute = false;
 
-        string returned = String.Empty;
+        //std::cout Reciever | handles console output
         using (AutoResetEvent outputWaitHandle = new AutoResetEvent(false))
         {
             proc.OutputDataReceived += (sender,e) => {
@@ -36,18 +46,21 @@ public class CalculatorExecutor : MonoBehaviour
                     outputWaitHandle.Set();
                 else
                 {
-                    UnityEngine.Debug.Log("Returned output: [ " + e.Data.ToString() + " ]");;
+                    UnityEngine.Debug.Log("Returned output: [ " + e.Data.ToString() + " ]");
                     returned = e.Data.ToString();
                 }
             };
         }
 
         proc.Start();
-        proc.WaitForExit();
+        CalculationHandler();
+    }
 
-
+    private async void CalculationHandler()
+    {
         proc.BeginOutputReadLine();
-        Thread.Sleep(1);
+        while(returned == String.Empty)
+            await Task.Yield();
 
         returnObject.text = returned;
     }
