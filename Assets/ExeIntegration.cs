e56b8b97f5133e0c77d04246eaefe6d7668bcc38
Timur Chelter <timur.chelter@mail.ru>
2023-02-08 18:52:38 +0300

    week34 addon
    / added executor template object support
    / added the example for it

--- Assets/ExeIntegration.cs
+++ Assets/ExeIntegration.cs
@@ -8,46 +8,120 @@ using UnityEngine;
 public class ExeIntegration : MonoBehaviour
 {
     private static List<string> localResults;
-    static public void ExeIntegrate(string exePath, string[] args, Action<List<string>> endFunction)
+    static public void ExeIntegrate(
+        string              exePath,
+        string[]                args,
+        Action<List<string>>    endFunction)
     {
-        Process proc = new Process();
-        localResults = new List<string>(){};
 
-        //Setting up .exe path and passing arguments to it
-        proc.StartInfo.FileName = exePath;
+        //Setting up process and local properties
+        localResults  =      new List<string>(){};
+        Process proc  =      new Process
+        {
+            StartInfo =
+            {
+                FileName = exePath,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            }
+        };
+
+        //Setting up process arguments
         foreach (string arg in args)
             proc.StartInfo.Arguments += arg + " ";
 
-        UnityEngine.Debug.Log("[ Exe file path: "+ proc.StartInfo.FileName +" ] | [ " + "Passed args: " + proc.StartInfo.Arguments + " ]");
-
-                //Setting up process properties
-        proc.StartInfo.CreateNoWindow = true;
-        proc.StartInfo.RedirectStandardOutput = true;
-        proc.StartInfo.UseShellExecute = false;
-
+        //Setting up output handle event
         using (AutoResetEvent outputWaitHandle = new AutoResetEvent(false))
         {
             proc.OutputDataReceived += (sender,e) => {
                 if (e.Data == null)
                     outputWaitHandle.Set();
                 else
-                {
                     localResults.Add(e.Data);
-                }
             };
         }
 
+        //Starting the process and waiting for an answer
         proc.Start();
         SessionHandler(proc, endFunction);
     }
 
-    private static async void SessionHandler(Process proc, Action<List<string>> endFunction)
+    private static async void SessionHandler(
+        Process                 proc,
+        Action<List<string>>    endFunction)
     {
+        //Starting the output handler
         proc.BeginOutputReadLine();
+
+        //Waiting till its done
         while(!proc.WaitForExit(10))
             await Task.Yield();
 
+        //Running passed end function and after that deleting results
         endFunction(localResults);
+        localResults = null;
+    }
+
+
+    //
+    //
+    //
+
+    static public void ExeIntegrate<T>(
+        string                  exePath,
+        string[]                args,
+        Action<T>    endFunction,
+        Func<List<string>,T>    castToTemplate)
+    {
+
+        //Setting up process and local properties
+        localResults  =      new List<string>(){};
+        Process proc  =      new Process
+        {
+            StartInfo =
+            {
+                FileName = exePath,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            }
+        };
+
+        //Setting up process arguments
+        foreach (string arg in args)
+            proc.StartInfo.Arguments += arg + " ";
+
+        //Setting up output handle event
+        using (AutoResetEvent outputWaitHandle = new AutoResetEvent(false))
+        {
+            proc.OutputDataReceived += (sender,e) => {
+                if (e.Data == null)
+                    outputWaitHandle.Set();
+                else
+                    localResults.Add(e.Data);
+            };
+        }
+
+        //Starting the process and waiting for an answer
+        proc.Start();
+        SessionHandler<T>(proc, endFunction, castToTemplate);
+    }
+
+    private static async void SessionHandler<T>(
+        Process                 proc,
+        Action<T>    endFunction,
+        Func<List<string>,T>    castToTemplate)
+    {
+        //Starting the output handler
+        proc.BeginOutputReadLine();
+
+        //Waiting till its done
+        while(!proc.WaitForExit(10))
+            await Task.Yield();
+
+        //Running passed end function and after that deleting results
+        endFunction(castToTemplate(localResults));
         localResults = null;
     }
 
