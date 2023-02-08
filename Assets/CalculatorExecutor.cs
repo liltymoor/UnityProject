9b450367318a85af33fa1a7f5036a6c02c322387
Timur Chelter <timur.chelter@mail.ru>
2023-02-08 19:03:57 +0300

    week34 addon
    / added executor template object support
    / added the example for it

--- Assets/CalculatorExecutor.cs
+++ Assets/CalculatorExecutor.cs
@@ -2,6 +2,25 @@ using UnityEngine;
 using System.Collections.Generic;
 
 
+
+public class CalcResultObj
+{
+    string val;
+    public CalcResultObj(string value = "")
+    {
+        val = value;
+    }
+
+    public string GetValue()
+    {
+        return val;
+    }
+    public static CalcResultObj ParseConsoleOutput(List<string> output)
+    {
+        return new CalcResultObj(output[0]);
+    }
+}
+
 public class CalculatorExecutor : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI returnObject;
@@ -10,21 +29,23 @@ public class CalculatorExecutor : MonoBehaviour
     public TMPro.TextMeshProUGUI value2;
     public TMPro.TMP_Dropdown operation;
 
+
     public void ExeCalculate()
     {
         ExeIntegration.ExeIntegrate(
             Application.dataPath + "/Resources/C++/calculator.exe",
             new string[]
             {
-                "!" + operation.options[operation.value].text,
-                value1.GetParsedText().Remove(value1.GetParsedText().Length - 1),
-                value2.GetParsedText().Remove(value2.GetParsedText().Length - 1)
+                "!" + operation.options[operation.value].text,                      // operation
+                value1.GetParsedText().Remove(value1.GetParsedText().Length - 1),   // left num
+                value2.GetParsedText().Remove(value2.GetParsedText().Length - 1)    // right num
             },
-            UpdateUI);
+            UpdateUI,
+            CalcResultObj.ParseConsoleOutput);
     }
 
-    private void UpdateUI(List<string> result)
+    private void UpdateUI(CalcResultObj obj)
     {
-        returnObject.text = result[0];
+        returnObject.text = obj.GetValue();
     }
 }
